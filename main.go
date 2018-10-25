e39e481829c09f6e502e88fdd26361db73f2ef5c
Andrea Bianchi <itab1@CPECMMAC005.local>
2018-10-25 17:05:16 +0100

    client

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"time"
 )
+
+const localhost = "http://localhost:8080/chain"
 
 type Block struct {
 	ID        string
@@ -38,4 +42,31 @@ func (c Chain) Add(block Block) error {
 
 func main() {
 	fmt.Println(Hash(Block{"123", "456", []byte("test data"), time.Now()}))
+}
+
+func AddBlock(c http.Client, b Block) error {
+	buff := bytes.NewReader{}
+	enc := json.NewEncoder(buff)
+	enc.Encode(b)
+	bJSON, _ := json.Marshal(b)
+	req, err := http.NewRequest(http.MethodPost, localhost, buff)
+	resp, err := c.Do(req)
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("received %d instead of 200", resp.StatusCode)
+	}
+	return err
+}
+
+func GetChain(c http.Client) (Chain, error) {
+	resp, err := http.Get(localhost)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("received %d instead of 200", resp.StatusCode)
+	}
+	dec := json.NewDecoder(resp.Body)
+	var chain Chain
+	err = dec.Decode(chain)
+	return chain, err
 }
